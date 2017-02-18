7bb14a782fac970b70da1717645f46245a9d3657
Winston Delos Santos <supergithubo@users.noreply.github.com>
2017-02-18 21:49:59 +0800

    Update Schalter.php

--- Schalter.php
+++ Schalter.php
@@ -3,14 +3,16 @@
 class Schalter {
     
     public $data;
-    private $domain;
-    private $identity;
     
     public function __construct($json, $domain = null, $identity = null)
     {
-        $this->domain = $domain;
-        $this->identity = $identity;
-        $this->data = json_decode(file_get_contents($json));
+        $remote = json_decode(file_get_contents($json));
+        if($remote[$domain]) {
+            $this->data = $remote[$domain];   
+        }
+        if($remote[$identity]) {
+            $this->data = $remote[$identity];   
+        }
     }
     
 }
